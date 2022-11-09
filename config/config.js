802bfeb8c848ee48a7f6a005d279836ee8d5aadf
ArTecDAO <116901052+ArTecDAO@users.noreply.github.com>
2022-11-10 03:57:32 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ const config = {
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
     ga: '',
-    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
+    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/logo.png',
     item_path_name: 'punk',
     page_item_num: 60,
     content_image_is_video: false,
